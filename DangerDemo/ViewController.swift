ef21abd77f46c03b8cd70f2660ebef059e00d267
RomanTysiachnik <roman.tysiachnik@oath.com>
2018-11-27 18:43:57 +0200

    Initial project commit

---
+++ DangerDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  DangerDemo
+//
+//  Created by rtysiachnik on 11/27/18.
+//  Copyright © 2018 rtysiachnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem 'fastlane'
+gem 'danger'
+gem 'danger-xcov'
+gem 'danger-swiftlint'
+gem 'danger-todoist'
+gem 'xcov'
